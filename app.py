1baa2e26b504625236acdd0be19f5a88d8133672
anusree2007prasannan-art <anusree2007prasannan@gmail.com>
2026-07-20 11:55:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import numpy as np
+import pickle
+import base64
+try:
+    model=pickle.load(open("house_model.pkl","rb"))
+except Exception as e:
+    st.error(f"Error loading model: {e}")
+
+ # keeping background image
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image:
+          encoded=base64.b64encode(image.read()).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image:url("data:image/jpeg;base64,{encoded}");
+            background-size:cover; 
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+add_bg_from_local("Houseimage.jpeg")
+
+st.title("🏡Dream House")
+st.subheader("House Price Prediction App")
+st.markdown("This app predicts the price of your Dream House using Machine Learning Algorithm, LinearRegression")
+st.divider()
+st.markdown("<p style='font-size:25px;'>Enter the details below.</p>", unsafe_allow_html=True)
+st.markdown(""" 
+<style> 
+label {
+    font-size: 30px !important;
+    font-weight: bold;
+}
+</style>
+""", unsafe_allow_html=True)
+area=st.number_input("**Area in sqft**")
+area=int(area)
+bedrooms=st.number_input("**Number Of Bedrooms**")
+bedrooms=int(bedrooms)
+bathrooms=st.number_input("**Number Of Bathrooms**")
+bathrooms=int(bathrooms)
+stories=st.number_input("**Number Of Stories**")
+stories=int(stories)
+parking=st.number_input("**Number Of Parking area**")
+parking=int(parking)
+feature=[[area , bedrooms , bathrooms , stories , parking]]
+
+# Initialize session state
+with st.form("predicted_form"):
+    submitted = st.form_submit_button("💰**Predict Price**")
+
+if submitted:
+    if area > 0:  #if statement to check whether the input is valid or not.
+        Prediction=model.predict(feature)
+        st.session_state['predicted']=True
+        st.session_state['price']=Prediction[0]
+    else:
+        st.error("Oops😟, Try Again")
+        st.session_state['predicted'] = False
+
+# 2. Show result + slider only if we have predicted
+if st.session_state.get('predicted', False):
+        st.success(f"**Your Predicted Price will be: ₹{st.session_state['price']:,.2f}**")
+        st.write("**Thank You for spending your time with us🙏.**")
+
+        # 4. Slider with key + f-string to show stars
+        rating=st.slider("**Rate this Prediction**", 1, 5, value=1, step = 1, key="rating_slider")
+        st.write(f"**Your Rating:** ","⭐" * rating)   # <- f before " is important
+    
+
+    
